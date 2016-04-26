c3948de6381dd267b1819cb2d2fca28551beb51f
fernando villamor jr <fervillz@gmail.com>
2016-04-26 13:05:07 -0700

    2nd files

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 </head>
-
-<body>
-
+<?php $baseUrl = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; ?>
+<body style="background-image: url('<?php echo $imgSrc = $baseUrl."images/bg".(rand(1,7)).".jpg"; ?>');"> 
 	<div class="container">
 		<header>
 			<div class="branding">
@@ -19,10 +18,8 @@
 				</div><!-- .logo -->
 				<div class="navigation">
 					<ul>
-						<li><a href="">Contact</a></li>
-						<li><a href="">Freebies</a></li>
-						<li><a href="">Plugins</a></li>
-						<li><a href="">Themes</a></li>
+						<li><a href="#modal">Contact</a></li>
+						<li><a href="">WP Themes/Plugins</a></li>
 						<li class="active"><a href="">Home</a></li>
 					</ul>
 				</div><!-- .navigation -->
@@ -31,14 +28,14 @@
 		<div class="page">
 			<div class="hero">
 				<div class="hero-title">
-					We're Creative Studio from Bukidnon
+					We're Creative Wordpress Studio from Bukidnon <a href="#openmap" class="map">locate us</a>
 				</div><!-- .hero-title -->
 				<div class="hero-desc">
 					<p>Our website is under construction. We'll be here soon with our new and amazing site. Click 'Update me' to get notified.</p>
 				</div><!-- .hero-desc -->
 				<div class="hero-buttons">
-					<a href="" class="button large transparent">Update Me</a>
-					<a href="" class="button large transparent">Learn More</a>
+					<a href="#modal" class="button large transparent">Update Me</a>
+					<a href="#modal" class="button large transparent">Learn More</a>
 				</div><!-- .hero-buttons -->
 			</div><!-- .hero -->
 		</div><!-- .page -->
@@ -55,6 +52,15 @@
 			</div><!-- .footer-social -->
 		</footer>
 	</div><!-- .container -->
+	<div id="modal" class="modal box">
+		<h1>Contact Us</h1>
+		<p>Reply varies differently on timezone.</p>
+		<form action="">
+			<input type="email" placeholder="Enter your email address" />
+			<input type="submit" />
+		</form>
+		<a href="#close" class="closed">close</a>
+	</div><!-- .modal box -->
 </body>
 
 </html>
